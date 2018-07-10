57901f0044a1f1771e3f490064a664471a00bb74
Ramiro Ruiz Larrabure <ramiro.ruiz@tecnotramit.com>
2018-07-10 16:38:57 +0200

    add server_name

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,7 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-server :local, roles: %w{app db web}
+server :local, roles: %w{app db web}, user: 'vagrant'
 
 # role-based syntax
 # ==================
